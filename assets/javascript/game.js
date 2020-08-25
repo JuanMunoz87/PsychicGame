e2ef8e0b44a63c7fda15817f1e3f7ed18bc7a6a2
JuanMunoz87 <jprmd87@gmail.com>
2020-08-24 22:04:52 -0400

    code is finish

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,3 @@
-// The array of all the alphabet
-const compLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-// A random generator 
-let randomLetter = compLetter[Math.floor(Math.random() * compLetter.length)];
-console.log(randomLetter);
-
-// Variables
-let wins = 0;
-let losses = 0;
-let remainingGuess = 4;
-let playerGuessesSoFar = [];
-
 // References
 const body = document.getElementsByTagName("body");
 const winsText = document.getElementById("wins");
@@ -18,67 +5,78 @@ const lossesText = document.getElementById("losses");
 const remainGuess = document.getElementById("remainGuesses");
 const userGuessSoFarText = document.getElementById("actualGuesses");
 
+
+// The array of all the alphabet letters
+const compLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// Variables
+let wins = 0;
+let losses = 0;
+let remainingGuess = 4;
+let playerGuessesSoFar = [];
+
+
 //=================== UPDATES =====================//
 // Update the player's wins
 
 const scoreUpdate = function() {
     winsText.innerHTML = wins;
 };
-scoreUpdate();
+//scoreUpdate();
 
 // Update the losses
 const loseUpdate = function() {
     lossesText.innerHTML = losses;
 };
-loseUpdate();
+//loseUpdate();
 
 const remainGuessUpdate = function() {
     remainGuess.innerHTML = remainingGuess;
 };
-remainGuessUpdate();
+//remainGuessUpdate();
 
 // Update the the player guess
 const guessesUpdate = function() {
     userGuessSoFarText.innerHTML = playerGuessesSoFar.join(" ");
 };
-guessesUpdate();
+//guessesUpdate();
 
-//let compLetter = ["a", "b", "c",];
-//let randomLetter = compLetter[Math.floor(Math.random() * compLetter.length)];
+// Random letter generator
+let randomLetter = compLetter[Math.floor(Math.random() * compLetter.length)];
+
 const reset = function() {
-    maxGuesses = 3;
-    remainingGuess = 3;
+    remainingGuess = 4;
     playerGuessesSoFar = [];
     randomLetter = compLetter[Math.floor(Math.random() * compLetter.length)];
-    console.log(randomLetter);
+    guessesUpdate();
+    remainGuessUpdate();
 };
-
 
 //================== Main Progress ================//
 // key event function
 document.onkeyup = function(event) {
 
-    remainingGuess--;
-
     let playerGuess = userGuessSoFarText.textContent = event.key;
+
+    remainingGuess--;
 
     playerGuessesSoFar.push(playerGuess);
 
     guessesUpdate();
     remainGuessUpdate();
 
-
-    if(playerGuess === randomLetter) {
+    if(remainingGuess > 0) {
+        if(playerGuess == randomLetter) {
         wins++;
         scoreUpdate();
         reset();
+        }
     }
-    else if(remainingGuess === 0) {
+    else if(remainingGuess == 0) {
         losses++;
         loseUpdate();
         reset();
     }
-
 };
 
 
